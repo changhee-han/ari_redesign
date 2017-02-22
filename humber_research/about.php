09af46adcc015073a23f7d9f716f75d9d6d907ce
changhee-han <changhee.han@humber.ca>
2017-02-22 14:36:58 -0500

    Add files via upload

---
+++ humber_research/about.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: about
+*/
+
+echo "this is about page";
+
+
+?>
---
+++ humber_research/footer.php
@@ -0,0 +1,10 @@
+    <footer>
+        <p>Put your footer here</p>
+        <?php wp_nav_menu(array('theme_location' == 'secondary')); ?>
+
+
+    </footer>
+    <?php wp_footer(); ?>
+    </body>
+</html>
+    
