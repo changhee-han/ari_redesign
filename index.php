81e58097371122741371f1cb210d6bcc72d9e10d
changhee-han <changhee.han@humber.ca>
2017-03-20 23:19:00 -0400

    fixed not showing facts banner

--- index.php
+++ index.php
@@ -60,87 +60,81 @@ else : // no rows found
 endif;
 ?>
 
-
 <?php
 //Articles Banner
-
-// check if the repeater field has rows of data
-if( have_rows('article_banner') ): ?>
-    <?php $category = ['latest-news', 'success-stories'];
-          $i = 0;
+if( have_rows('article_banner') ):
+    $category = array('latest-news', 'success-stories');
+    $i = 0;
     ?>
-    <div class="section article_banner">
-    <?php while ( have_rows('article_banner') ) : the_row(); ?>
+    <?php
+    // loop through the rows of data
+    while ( have_rows('article_banner') ) : the_row(); ?>
 
         <?php if (get_sub_field('article_banner_display_on_off')): ?>
-            <div class="articles_wrapper">
-                <h3 class="article_section_headline"><?php the_sub_field('article_section_headline'); ?></h3>
-                <div class="article_box">
-                    <?php
-                    global $post;
-                    $args = array( 'numberposts' => 1, 'category_name' => $category[$i] );
-                    $posts = get_posts( $args );
-                    foreach( $posts as $post ): setup_postdata($post);?>
-                        <img src="<?php echo catch_that_image(); ?>" alt="<?php the_title(); ?>"/>
-                        <p class="article_title"><?php the_title(); ?></p>
-                        <p class="article_content"><?php the_excerpt(); ?></p>
-<!--                        <p>--><?php //the_content(); ?><!--</p>-->
-                    <?php endforeach; ?>
-                </div>
-            <?php if( have_rows('more_button') ): // check if the repeater field has rows of data ?>
-                <?php while ( have_rows('more_button') ) : the_row(); // loop through the rows of data ?>
-                    <a class="more_button" href="<?php the_sub_field('more_button_link_url'); ?>">
-                        <?php the_sub_field('more_button_title'); ?>
-                    </a>
-                <?php endwhile; ?>
-            <?php endif; ?>
-            </div>
+            <?php
+            global $post;
+            $args = array( 'posts_per_page' => 1, 'category' => $category[$i] );
+            $lastposts = get_posts( $args );
+            foreach ( $lastposts as $post ) :
+                setup_postdata( $post ); ?>
+                <img src="<?php echo catch_that_image(); ?>" alt="<?php the_title(); ?>"/>
+                <p class="article_title"><?php the_title(); ?></p>
+                <p class="article_content"><?php the_excerpt(); ?></p>
+                <a class="more_button" href="<?php the_permalink(); ?>">Read More</a>
+            <?php endforeach;
+            wp_reset_postdata(); ?>
+<!--                --><?php //if( have_rows('more_button') ): // check if the repeater field has rows of data ?>
+<!--                    --><?php //while ( have_rows('more_button') ) : the_row(); // loop through the rows of data ?>
+<!--                        <a class="more_button" href="--><?php //the_sub_field('more_button_link_url'); ?><!--">-->
+<!--                            --><?php //the_sub_field('more_button_title'); ?>
+<!--                        </a>-->
+<!--                    --><?php //endwhile; ?>
+<!--                --><?php //endif; ?>
+            <?php $i++; ?>
         <?php endif; ?>
-        <?php $i++; ?>
     <?php endwhile; ?>
-
-<?php else : // no rows found ?>
-    </div>
 <?php endif; ?>
 
+
+
 <?php
+
     //Media Banner
-    if( have_rows('media_banner') ): // check if the repeater field has rows of data ?>
+if( have_rows('media_banner') ): // check if the repeater field has rows of data
+    $i = 0;?>
+    <?php while ( have_rows('media_banner') ) : the_row(); // loop through the rows of data ?>
         <?php if (get_sub_field('media_banner_display_on_off')): ?>
 
-        <div class="section media_banner">
-        <?php while ( have_rows('media_banner') ) : the_row(); // loop through the rows of data ?>
+    <div class="section media_banner">
+        <?php if( have_rows('media_resources') ): // display a sub field value & check if the repeater field has rows of data ?>
+            <?php $i = 0;?>
+            <?php while ( have_rows('media_resources') ) : the_row(); // loop through the rows of data ?>
+                <?php if (get_sub_field('media_display_on_off')): ?>
+                <div class="media" id="media_<?php echo $i;?>">
+                    <a href="<?php the_sub_field('media_link_url'); ?>">
 
-            <?php if( have_rows('media_resources') ): // display a sub field value & check if the repeater field has rows of data ?>
-                <?php $i = 0;?>
-                <?php while ( have_rows('media_resources') ) : the_row(); // loop through the rows of data ?>
-                    <?php if (get_sub_field('media_display_on_off')): ?>
-                    <div class="media" id="media_<?php echo $i;?>">
-                        <a href="<?php the_sub_field('media_link_url'); ?>">
-
-                        <?php if(get_sub_field('media_type') == "Image") { ?>
-                            <img src="<?php the_sub_field('media_file'); ?>" alt=""/>
-                        <?php } ?>
-                        <?php if(get_sub_field('media_type') == "Video") { ?>
-                            <video src="<?php the_sub_field('media_file'); ?>"></video>
-                        <?php } ?>
-                        </a>
-                    </div>
-                    <?php $i++;?>
+                    <?php if(get_sub_field('media_type') == "Image") : ?>
+                        <img src="<?php the_sub_field('media_file'); ?>" alt=""/>
                     <?php endif; ?>
-                <?php endwhile; ?>
+                    <?php if(get_sub_field('media_type') == "Video") : ?>
+                        <video src="<?php the_sub_field('media_file'); ?>"></video>
+                        <?php endif; ?>
+                    </a>
+                </div>
+                <?php $i++;?>
+                <?php endif; ?>
+            <?php endwhile; ?>
 
 
         <?php
-            else : // no rows found
             endif;
 
-        endwhile; ?>
+         ?>
         </div>
 
 <?php
         endif;
-    else : // no rows found
+        endwhile;
     endif;
 ?>
 
@@ -149,29 +143,31 @@ if( have_rows('article_banner') ): ?>
 
 // check if the repeater field has rows of data
 if( have_rows('facts_banner') ): ?>
-    <div class="section facts_banner">
+
     <?php while ( have_rows('facts_banner') ) : the_row(); ?>
 
         <?php if (get_sub_field('fact_banner_display_on_off')): ?>
-        <div class="facts_wrapper">
-        <?php if( have_rows('fact_info') ): ?>
-            <?php while ( have_rows('fact_info') ) : the_row(); ?>
+        <div class="section facts_banner">
+            <div class="facts_wrapper">
+            <?php if( have_rows('fact_info') ): ?>
+                <?php while ( have_rows('fact_info') ) : the_row(); ?>
 
-                <?php if (get_sub_field('fact_display_on_off')): ?>
-                <div class="fact_box">
-                    <p class="fact_number"><?php the_sub_field('fact_number'); ?></p>
-                    <p class="fact_name"><?php the_sub_field('fact_title'); ?></p>
-                </div>
+                    <?php if (get_sub_field('fact_display_on_off')): ?>
+                    <div class="fact_box">
+                        <p class="fact_number"><?php the_sub_field('fact_number'); ?></p>
+                        <p class="fact_name"><?php the_sub_field('fact_title'); ?></p>
+                    </div>
 
-                <?php endif; ?>
-            <?php endwhile; ?>
-        <?php endif; ?>
+                    <?php endif; ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
+            </div>
         </div>
         <?php endif; ?>
     <?php endwhile; ?>
 
 <?php else : // no rows found ?>
-    </div>
+
 <?php endif; ?>
 
 <?php
@@ -180,13 +176,12 @@ if( have_rows('facts_banner') ): ?>
 // check if the repeater field has rows of data
 
 if( have_rows('partners_banner') ): ?>
-    <div class="section partners_banner">
+
     <?php
     // loop through the rows of data
     while ( have_rows('partners_banner') ) : the_row(); ?>
-
         <?php if (get_sub_field('partner_banner_display_on_off')): ?>
-
+        <div class="section partners_banner">
         <?php
         // display a sub field value
         // check if the repeater field has rows of data
@@ -209,12 +204,12 @@ if( have_rows('partners_banner') ): ?>
         <?php else : // no rows found ?>
             </div>
         <?php endif; ?>
-
+        </div>
         <?php endif; ?>
     <?php endwhile; ?>
 
 <?php else : // no rows found ?>
-    </div>
+
 <?php endif; ?>
 
 
@@ -244,5 +239,6 @@ if( have_rows('subscription_banner') ): ?>
     </div>
 <?php endif; ?>
 </div>
+
 
 <?php get_footer(); ?>
