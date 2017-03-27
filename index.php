186bf647718b1f34ce2eeced84e5762d6bafa724
changhee-han <changhee.han@humber.ca>
2017-03-27 19:53:52 -0400

    updated subscribe form

--- index.php
+++ index.php
@@ -233,6 +233,55 @@ Template Name: Home Page
                     the_sub_field('subscription_title'); ?></h2>
                 <p class="subscription_content"><?php the_sub_field('subscription_content'); ?></p>
 
+
+                <div class="subscribeForm">
+                    <!-- Begin MailChimp Signup Form -->
+                    <div id="mc_embed_signup">
+                        <form action="//humber.us15.list-manage.com/subscribe/post?u=ea5cd9dce6ed29c65f8fcc339&amp;id=623bb6f6d7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                            <div id="mc_embed_signup_scroll">
+<!--                                <h2>Subscribe to our mailing list</h2>-->
+<!--                                <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>-->
+                                <div class="mc-field-group">
+                                    <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+                                    </label>
+                                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                </div>
+                                <div class="mc-field-group">
+                                    <label for="mce-FNAME">First Name </label>
+                                    <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+                                </div>
+                                <div class="mc-field-group">
+                                    <label for="mce-LNAME">Last Name </label>
+                                    <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+                                </div>
+                                <div class="mc-field-group">
+                                    <label for="mce-MMERGE4">Are you a student, faculty member, or external?  <span class="asterisk">*</span>
+                                    </label>
+                                    <select name="MMERGE4" class="required" id="mce-MMERGE4">
+                                        <option value=""></option>
+                                        <option value="Student">Student</option>
+                                        <option value="Faculty">Faculty</option>
+                                        <option value="External">External</option>
+
+                                    </select>
+                                </div>
+                                <div class="mc-field-group">
+                                    <label for="mce-MMERGE3">Company (if applicable) </label>
+                                    <input type="text" value="" name="MMERGE3" class="" id="mce-MMERGE3">
+                                </div>
+                                <div id="mce-responses" class="clear">
+                                    <div class="response" id="mce-error-response" style="display:none"></div>
+                                    <div class="response" id="mce-success-response" style="display:none"></div>
+                                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_ea5cd9dce6ed29c65f8fcc339_623bb6f6d7" tabindex="-1" value=""></div>
+                                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <!--End mc_embed_signup-->
+                </div>
+
             <?php endif; ?>
         <?php endwhile; ?>
 
